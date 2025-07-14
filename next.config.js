10b6058479631b705456b57a6f8ab3389ed11503
Amarendra Das <amarendra.das999@gmail.com>
2025-07-14 23:32:50 +0530

    builderr_fix_v0.7

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import path from "path";
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
@@ -12,6 +13,10 @@ const config = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default config;
